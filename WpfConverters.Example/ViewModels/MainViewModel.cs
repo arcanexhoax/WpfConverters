b59205d54b98dbbfea5711debd47465a953afb56
hoax <ninjaa176@gmail.com>
2024-01-21 23:52:01 +0200

    add type converter

--- WpfConverters.Example/ViewModels/MainViewModel.cs
+++ WpfConverters.Example/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ namespace WpfConverters.Example.ViewModels
     internal class MainViewModel : BindableBase
     {
         private double _width;
+        private bool _boolValue;
 
         public double Width
         {
@@ -12,9 +13,16 @@ namespace WpfConverters.Example.ViewModels
             set => SetProperty(ref _width, value);
         }
 
+        public bool BoolValue
+        {
+            get => _boolValue;
+            set => SetProperty(ref _boolValue, value);
+        }
+
         public MainViewModel()
         {
             Width = 100;
+            BoolValue = true;
         }
     }
 }
---
+++ WpfConverters/Converters/TypeConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+
+namespace WpfConverters.Converters
+{
+    public class TypeConverter : ConverterBase
+    {
+        /// <summary>
+        /// Target type that value converts in
+        /// </summary>
+        public Type To { get; set; }
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            object result = DependencyProperty.UnsetValue;
+
+            if (value is IConvertible || value == null)
+            {
+                result = System.Convert.ChangeType(value, To, culture);
+            }
+            else
+            {
+                var typeConverter = TypeDescriptor.GetConverter(value);
+
+                if (typeConverter.CanConvertTo(To))
+                    result = typeConverter.ConvertTo(null, culture, value, To);
+            }
+
+            return ConvertNextIfNeeded(result);
+        }
+    }
+}
