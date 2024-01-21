edf8d29b3de5714d36e7909eaad68bf75d4724e5
hoax <ninjaa176@gmail.com>
2024-01-22 01:22:53 +0200

    add collection count converter

---
+++ WpfConverters/Converters/Collections/CollectionToCountConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows;
+
+namespace WpfConverters.Converters
+{
+    public class CollectionToCountConverter : ConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return DependencyProperty.UnsetValue;
+
+            int count = 0;
+
+            if (value is ICollection collection)
+                count = collection.Count;
+            else if (value is object[] array)
+                count = array.Length;
+            else if (value is IEnumerable enumerable)
+            {
+                var e = enumerable.GetEnumerator();
+
+                while (e.MoveNext())
+                    count++;
+            }
+            else
+            {
+                throw new ArgumentException("The given value is not a collection.");
+            }
+
+            return ConvertNextIfNeeded(count);
+        }
+    }
+}
