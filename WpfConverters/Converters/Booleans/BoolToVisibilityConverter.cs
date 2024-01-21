9044365d3f4b4e1962aae8b43babfc664d716973
hoax <ninjaa176@gmail.com>
2024-01-21 18:19:05 +0200

    add custom bool to visibility converter

---
+++ WpfConverters/Converters/Booleans/BoolToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfConverters.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BoolToVisibilityConverter : ConverterBase
+    {
+        /// <summary>
+        /// The <see cref="Visibility"/> behavior when binding value is <see langword="true"/>. Default value is <see cref="Visibility.Visible"/>.
+        /// </summary>
+        public Visibility ForTrue { get; set; } = Visibility.Visible;
+
+        /// <summary>
+        /// The <see cref="Visibility"/> behavior when binding value is <see langword="false"/>. Default value is <see cref="Visibility.Collapsed"/>.
+        /// </summary>
+        public Visibility ForFalse { get; set; } = Visibility.Collapsed;
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool input = System.Convert.ToBoolean(value);
+            Visibility result = input ? ForTrue : ForFalse;
+
+            return ConvertNextIfNeeded(result);
+        }
+    }
+}
