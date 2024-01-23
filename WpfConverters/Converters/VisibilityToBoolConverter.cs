7b59fcbd30c67e6d80f2494e741b94c04d956fd8
hoax <ninjaa176@gmail.com>
2024-01-23 12:14:11 +0200

    add visibility to bool converter

---
+++ WpfConverters/Converters/VisibilityToBoolConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace WpfConverters.Converters
+{
+    public class VisibilityToBoolConverter : ConverterBase
+    {
+        /// <summary>
+        /// Bool value for <see cref="Visibility.Visible"/> state. Default if <see cref="true"/>.
+        /// </summary>
+        public bool ForVisible { get; set; } = true;
+
+        /// <summary>
+        /// Bool value for <see cref="Visibility.Hidden"/> state. Default if <see cref="false"/>.
+        /// </summary>
+        public bool ForHidden { get; set; } = false;
+
+        /// <summary>
+        /// Bool value for <see cref="Visibility.Collapsed"/> state. Default if <see cref="false"/>.
+        /// </summary>
+        public bool ForCollapsed { get; set; } = false;
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not Visibility visibility)
+                throw new ArgumentException($"Given value has no {nameof(Visibility)} type.");
+
+            bool result = visibility switch
+            {
+                Visibility.Visible => ForVisible,
+                Visibility.Hidden  => ForHidden,
+                _                  => ForCollapsed,
+            };
+
+            return ConvertNextIfNeeded(result);
+        }
+    }
+}
