b5713f57b8daa8905fb4a37f78d72e4f4a79f4d6
hoax <ninjaa176@gmail.com>
2024-01-23 13:17:44 +0200

    improve string converter

--- WpfConverters/Converters/Strings/StringFormatConverter.cs
+++ WpfConverters/Converters/Strings/StringFormatConverter.cs
@@ -1,35 +1,30 @@
 ﻿using System;
-using System.Collections.ObjectModel;
+using System.Collections;
 using System.Globalization;
 using System.Linq;
-using System.Windows.Data;
 
 namespace WpfConverters.Converters
 {
-    [ValueConversion(typeof(string), typeof(string))]
     public class StringFormatConverter : ConverterBase
     {
         /// <summary>
-        /// The argument to format.
+        /// String format template for the given value(-s). 
         /// </summary>
-        public string Argument { get; set; }
-
-        /// <summary>
-        /// The arguments to format. They will be used only if <see cref="Argument"/> is not set.
-        /// </summary>
-        public Collection<string> Arguments { get; } = [];
+        public string Format { get; set; } = string.Empty;
 
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            string input = targetType == typeof(string) ? (string)value : value.ToString();
             string result;
 
-            if (Argument is not null)
-                result = string.Format(input, Argument);
-            else if (Arguments is not null && Arguments.Count > 0)
-                result = string.Format(input, args: Arguments.ToArray());
+            if (value is IEnumerable enumerable)
+            {
+                var castedEnumerable = enumerable.Cast<object>();
+                result = string.Format(Format, castedEnumerable.ToArray());
+            }
             else
-                throw new ArgumentException($"No of the parameters ({nameof(Argument)} or {nameof(Arguments)}) were set.");
+            {
+                result = string.Format(Format, value);
+            }
 
             return ConvertNextIfNeeded(result);
         }
