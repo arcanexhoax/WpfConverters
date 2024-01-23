355d44ec8d44bb3dd5508f7f656250da2a362b0e
hoax <ninjaa176@gmail.com>
2024-01-24 00:42:18 +0200

    add url to image converter

---
+++ WpfConverters/Converters/UrlToImageConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WpfConverters.Converters
+{
+    public class UrlToImageConverter : ConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = new BitmapImage();
+            result.BeginInit();
+            result.CacheOption = BitmapCacheOption.OnLoad;
+
+            Uri sourceUri = value switch
+            {
+                string stringUri => new Uri(stringUri, UriKind.RelativeOrAbsolute),
+                Uri uri          => uri,
+                _                => null
+            };
+
+            if (sourceUri is null)
+                return DependencyProperty.UnsetValue;
+
+            result.UriSource = sourceUri;
+            result.EndInit();
+            return ConvertNextIfNeeded(result);
+        }
+    }
+}
