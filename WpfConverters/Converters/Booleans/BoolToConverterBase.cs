010fce3e57a7a8afd23cb3fe99525236fc37ef88
hoax <ninjaa176@gmail.com>
2024-01-24 02:33:32 +0200

    add lots of bool to types converters

---
+++ WpfConverters/Converters/Booleans/BoolToConverterBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WpfConverters.Converters
+{
+    public class BoolToConverterBase<T> : ConverterBase
+    {
+        /// <summary>
+        /// The <see cref="T"/> behavior when binding value is <see langword="true"/>.
+        /// </summary>
+        public T ForTrue { get; set; }
+
+        /// <summary>
+        /// The <see cref="T"/> behavior when binding value is <see langword="false"/>.
+        /// </summary>
+        public T ForFalse { get; set; }
+
+        internal BoolToConverterBase()
+        {
+        }
+
+        /// <summary>
+        /// Base bool to <see cref="T"/> converter. 
+        /// </summary>
+        /// <param name="defaultTrue">Default value of <see cref="ForTrue"/> property</param>
+        /// <param name="defaultFalse">Default value of <see cref="ForFalse"/> property</param>
+        internal BoolToConverterBase(T defaultTrue, T defaultFalse)
+        {
+            ForTrue = defaultTrue;
+            ForFalse = defaultFalse;
+        }
+
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool input = System.Convert.ToBoolean(value);
+            T result = input ? ForTrue : ForFalse;
+
+            return ConvertNextIfNeeded(result);
+        }
+    }
+}
---
+++ WpfConverters/Converters/Booleans/BoolToConverters.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfConverters.Converters
+{
+    public class BoolToObjectConverter() : BoolToConverterBase<object>(null, null) { }
+    public class BoolToDoubleConverter() : BoolToConverterBase<double>(0, 0) { }
+    public class BoolToStringConverter() : BoolToConverterBase<string>(string.Empty, string.Empty) { }
+    public class BoolToVisibilityConverter() : BoolToConverterBase<Visibility>(Visibility.Visible, Visibility.Collapsed) { }
+    public class BoolToBrushConverter() : BoolToConverterBase<SolidColorBrush>(Brushes.Green, Brushes.Red) { }
+    public class BoolToThicknessConverter() : BoolToConverterBase<Thickness>(new Thickness(0), new Thickness(0)) { }
+    public class BoolToOpacityConverter() : BoolToConverterBase<double>(1, 0.5) { }
+}
